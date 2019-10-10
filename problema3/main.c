dff30e57d6387fcc35bd052781edd5eda1b5d7b3
Marlon <marlon56115@gmail.com>
2019-10-10 14:46:32 -0600

    manera iterativa

--- problema3/main.c
+++ problema3/main.c
@@ -27,10 +27,25 @@ int main(){
     printf("El calcularMCM de %d y %d es: %d", primero, segundo, resultado);
 
     /* Retornamos 0 para indicar que todo esta bien */
+    //ahora de manera interativa
+    int i,m,mcd;
+    printf("\nMaximo Comun divisor de 2 números forma iterativa:\n");
+    if (primero<segundo) {
+        m = primero;
+    } else {
+        m = segundo;
+    }
+    for (int i=1; i<=m;i++) {
+        if (primero%i==0 && segundo%i==0) {
+            mcd=i;
+        }
+    }
+    printf("El maximo comun divisor es: %3d" , mcd);
     return 0;
 }
 
 int calcularMCM(int m, int n) {
     if (n == 0) return m; // Condicion de parada
     return calcularMCM(n, m%n);   // Llamada recursiva
+
 }
